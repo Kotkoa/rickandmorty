787b11a615eb8c95caaf3e54a99ac305ba5edfff
Kotkoa <kotkoa@gmail.com>
2021-09-22 23:04:28 +0300

    Add the home page: header, footer, without buttons and body

--- src/Welcome.js
+++ src/Welcome.js
@@ -3,20 +3,23 @@ import { Link } from "react-router-dom"
 
 function Welcome() {
   return (
-    <div className="background">
-      <div className="overlay">
-        <div className="horizontal"></div>
-        <div className="rickandmorty"></div>
-        <h1 className="bien">Bienvenido a Rick and Morty</h1>
-        <p className="en">
-          En esta prueba, evaluaremos su capacidad para construir la aplicación
-          mediante el análisis de código y la reproducción del siguiente diseño.
-        </p>
-        <button className="btn">
-          <Link to="/home" className="continuar">
-            Continuar
-          </Link>
-        </button>
+    <div className="background_layer">
+      <div className="background">
+        <div className="overlay_layer">
+          <div className="suazo"></div>
+          <div className="rickandmorty"></div>
+          <h1 className="bien">Bienvenido a Rick and Morty</h1>
+          <p className="en">
+            En esta prueba, evaluaremos su capacidad para construir la
+            aplicación mediante el análisis de código y la reproducción del
+            siguiente diseño.
+          </p>
+          <button className="btn">
+            <Link to="/home" className="continuar">
+              Continuar
+            </Link>
+          </button>
+        </div>
       </div>
     </div>
   )
