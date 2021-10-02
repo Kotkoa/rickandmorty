a07ee9183f7e11281e8fa32176dcab53fc4c228b
Kotkoa <kotkoa@gmail.com>
2021-10-02 10:32:13 +0300

    char status light green or red

--- src/components/Charcard.js
+++ src/components/Charcard.js
@@ -38,7 +38,8 @@ function Charcard() {
                     <path
                       d="M8.10313 0.625785L6.06251 5.28047L1.49688 6.0293C0.678132 6.1629 0.350007 7.29844 0.943757 7.94883L4.24688 11.5699L3.46563 16.6852C3.32501 17.6098 4.19063 18.3024 4.91563 17.8699L9.00001 15.4547L13.0844 17.8699C13.8094 18.2988 14.675 17.6098 14.5344 16.6852L13.7531 11.5699L17.0563 7.94883C17.65 7.29844 17.3219 6.1629 16.5031 6.0293L11.9375 5.28047L9.89688 0.625785C9.53126 -0.203903 8.47188 -0.21445 8.10313 0.625785Z"
                       fill={
-                        +select.filter((it) => it === char.id).join() === char.id
+                        +select.filter((it) => it === char.id).join() ===
+                        char.id
                           ? "#F2994A"
                           : "#828282"
                       }
@@ -53,7 +54,9 @@ function Charcard() {
               onClick={() => dispatch(setDetails(char.id))}
             >
               <div className="charStatus">
-                <div className="sphereStatus"></div>
+                <div
+                  className={char.status === "Alive" ? "sphereStatus" : "sphereStatusred"}
+                ></div>
                 <div className="textStatus">
                   {char.status} - {char.species}
                 </div>
