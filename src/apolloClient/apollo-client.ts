a4d8954887b38fbfac8e12613ccfaefaf279f59c
Kotkoa <kotkoa@gmail.com>
2024-02-14 23:37:58 +0100

    update @types/node

--- src/apolloClient/apollo-client.ts
+++ src/apolloClient/apollo-client.ts
@@ -2,6 +2,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client';
 
 const host = process.env.VITE_REACT_APP_GRAPHQL_API;
 
+console.log(host);
+
 export const apolloClient = new ApolloClient({
   uri: host,
   cache: new InMemoryCache(),
