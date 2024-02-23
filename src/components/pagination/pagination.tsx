70add27c0835c492c90e13c468d1768331961073
Kotkoa <kotkoa@gmail.com>
2024-02-23 15:29:28 +0100

    Refactor pagination component to use activePageNumber and totalPages

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -18,14 +18,22 @@ export const Pagination: FC<PaginationI> = ({ pagination }) => {
 
   if (!pagination) return null;
 
-  const activePage = () => {
+  const totalPages = pagination.pages;
+
+  const activePageNumber = () => {
     if (!pagination.prev) {
       return 1;
     } else if (!pagination.next && pagination.pages) {
-      return pagination.pages;
+      return totalPages;
     } else if (pagination.next) {
       return pagination.next - 1;
     }
+  };
+
+  const handlePageChange = (page: number) => {
+    setPage(page);
+    searchParams.set('page', page.toString());
+    navigate(`${location.pathname}?${searchParams.toString()}`);
   };
 
   return (
@@ -34,29 +42,17 @@ export const Pagination: FC<PaginationI> = ({ pagination }) => {
         key="prevPage"
         type="button"
         disabled={!pagination.prev}
-        onClick={() => {
-          if (pagination.prev) {
-            setPage(pagination.prev);
-            searchParams.set('page', pagination.prev.toString());
-            navigate(`${location.pathname}?${searchParams.toString()}`);
-          }
-        }}>
+        onClick={() => pagination.prev && handlePageChange(pagination.prev)}>
         prev
       </button>
       <div>
-        Page {activePage} of {pagination.pages}
+        Page {activePageNumber()} of {totalPages}
       </div>
       <button
         key="nextPage"
         type="button"
         disabled={!pagination.next}
-        onClick={() => {
-          if (pagination.next) {
-            setPage(pagination.next);
-            searchParams.set('page', pagination.next.toString());
-            navigate(`${location.pathname}?${searchParams.toString()}`);
-          }
-        }}>
+        onClick={() => pagination.next && handlePageChange(pagination.next)}>
         next
       </button>
     </div>
