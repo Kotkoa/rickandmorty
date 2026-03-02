d92b08121164b96711160b713238a3ea8dd9c657
Kotkoa <kotkoa@gmail.com>
2026-03-02 21:51:56 +0100

    fix: update Apollo Client configuration and remove unused GraphQL type definitions

--- src/apolloClient/apollo-client.ts
+++ src/apolloClient/apollo-client.ts
@@ -1,9 +1,9 @@
-import { ApolloClient, InMemoryCache } from '@apollo/client';
+import { ApolloClient, HttpLink, InMemoryCache } from '@apollo/client';
 
 const host = import.meta.env.VITE_REACT_APP_GRAPHQL_API || 'https://rickandmortyapi.com/graphql';
 
 export const apolloClient = new ApolloClient({
-  uri: host,
+  link: new HttpLink({ uri: host }),
   cache: new InMemoryCache(),
   devtools: { enabled: import.meta.env.DEV },
 });
