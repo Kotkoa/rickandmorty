0d71252d690fecb1bf9a62703b552097770b0a0c
Kotkoa <kotkoa@gmail.com>
2026-03-02 21:46:49 +0100

    fix: update @apollo/client to version 4.1.6; improve GraphQL API host configuration

--- src/apolloClient/apollo-client.ts
+++ src/apolloClient/apollo-client.ts
@@ -1,9 +1,9 @@
 import { ApolloClient, InMemoryCache } from '@apollo/client';
 
-const host = import.meta.env.VITE_REACT_APP_GRAPHQL_API;
+const host = import.meta.env.VITE_REACT_APP_GRAPHQL_API || 'https://rickandmortyapi.com/graphql';
 
 export const apolloClient = new ApolloClient({
   uri: host,
   cache: new InMemoryCache(),
-  connectToDevTools: import.meta.env.DEV,
+  devtools: { enabled: import.meta.env.DEV },
 });
