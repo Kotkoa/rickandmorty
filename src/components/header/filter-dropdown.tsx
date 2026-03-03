a34f871f3842ecfb73110fc92b516e35b5e0581b
Kotkoa <kotkoa@gmail.com>
2026-03-03 13:01:38 +0100

    refactor: improve FilterDropdown structure and styles for better accessibility and usability

--- src/components/header/filter-dropdown.tsx
+++ src/components/header/filter-dropdown.tsx
@@ -37,43 +37,40 @@ export const FilterDropdown: FC = () => {
   };
 
   return (
-    <>
-      <div ref={dropdownRef} style={{ display: 'contents' }}>
-        <button
-          className={styles.toggleFilter}
-          aria-label="Filtros"
-          onClick={() => setIsDropdownVisible((prev) => !prev)}>
-          <Filtros className={styles.filtros} />
-        </button>
-        {isDropdownVisible && (
-          <div className={styles.dropDown}>
-            {listStatusFilters.map((statusValue) => (
-              <div key={statusValue}>
-                <input
-                  type="radio"
-                  id={`status-${statusValue}`}
-                  name={CharacterFiltersE.Status}
-                  value={statusValue}
-                  checked={status === statusValue}
-                  onChange={handleRadioChange}
-                />
-                <label htmlFor={`status-${statusValue}`}>{statusValue || 'Todos'}</label>
-              </div>
-            ))}
-            <div>
-              <input type="checkbox" id="origin" name="origin" checked={origin} onChange={handleCheckboxChange} />
-              <label htmlFor="origin">Origen conocido</label>
-            </div>
-          </div>
-        )}
-      </div>
-      {isFilterActive && (
-        <div className={styles.enabledFiltros}>
-          Filtros aplicados: {status && <p>Estado</p>}
-          {status && origin && <p>, </p>}
-          {origin && <p>Origen</p>}
+    <div ref={dropdownRef}>
+      <button
+        className={styles.toggleFilter}
+        aria-label="Filters"
+        onClick={() => setIsDropdownVisible((prev) => !prev)}>
+        <Filtros className={styles.filtros} />
+      </button>
+      {isDropdownVisible && (
+        <div className={styles.dropDown}>
+          {listStatusFilters.map((statusValue) => (
+            <label key={statusValue}>
+              <input
+                type="radio"
+                name={CharacterFiltersE.Status}
+                value={statusValue}
+                checked={status === statusValue}
+                onChange={handleRadioChange}
+              />
+              {statusValue || 'All'}
+            </label>
+          ))}
+          <label>
+            <input type="checkbox" checked={origin} onChange={handleCheckboxChange} />
+            Known origin
+          </label>
         </div>
       )}
-    </>
+      {isFilterActive && (
+        <div className={styles.enabledFiltros}>
+          Applied filters: {status && <span>Status</span>}
+          {status && origin && <span>, </span>}
+          {origin && <span>Origin</span>}
+        </div>
+      )}
+    </div>
   );
 };
