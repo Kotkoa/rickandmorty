7d71d5ab873fe328b00ccdde78be6ac11d33fa46
Kotkoa <kotkoa@gmail.com>
2026-03-03 13:34:12 +0100

    refactor: simplify Welcome component structure and update styles for better layout

--- src/components/welcome/welcome.tsx
+++ src/components/welcome/welcome.tsx
@@ -6,21 +6,15 @@ import rickAndMorty from '@/assets/rick-and-morty.png';
 
 import styles from './welcome.module.scss';
 
-export const Welcome: FC = () => {
-  return (
-    <div className={styles.background_layer}>
-      <div className={styles.background}>
-        <div className={styles.overlay_layer}>
-          <img src={horizontal} alt="suazo" />
-          <img src={rickAndMorty} alt="rick and morty" width={664} height={233} />
-          <h1>Bienvenido a Rick and Morty</h1>
-          <p>
-            En esta prueba, evaluaremos su capacidad para construir la aplicación mediante el análisis de código y la
-            reproducción del siguiente diseño.
-          </p>
-          <Link to="/home?page=1">Continuar</Link>
-        </div>
-      </div>
-    </div>
-  );
-};
+export const Welcome: FC = () => (
+  <main className={styles.background}>
+    <img src={horizontal} alt="Suazo" />
+    <img src={rickAndMorty} alt="Rick and Morty" width={664} height={233} />
+    <h1>Bienvenido a Rick and Morty</h1>
+    <p>
+      En esta prueba, evaluaremos su capacidad para construir la aplicación mediante el análisis de código y la
+      reproducción del siguiente diseño.
+    </p>
+    <Link to="/home?page=1">Continuar</Link>
+  </main>
+);
