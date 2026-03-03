95e9802b5fe8ffb064e356e58498399d35902228
Kotkoa <kotkoa@gmail.com>
2026-03-03 08:23:42 +0100

    refactor: add spacing for improved readability in app-router

--- src/routes/app-router.tsx
+++ src/routes/app-router.tsx
@@ -5,7 +5,9 @@ import { RouteErrorBoundary, RouteErrorFallback } from '@/components/route-error
 import { Root } from '@/root';
 
 const Welcome = lazy(() => import('src/components/welcome/welcome').then((m) => ({ default: m.Welcome })));
+
 const CharList = lazy(() => import('src/components/char-list/char-list').then((m) => ({ default: m.CharList })));
+
 const Ohno = lazy(() => import('src/components/oh-no/oh-no').then((m) => ({ default: m.Ohno })));
 
 const mainChildren = [
